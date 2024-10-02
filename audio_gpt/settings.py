50ecc05358c5ceef8738a8d7f5ebd9f9663af77a
mchoccie <minijm06@gmail.com>
2024-10-01 21:24:49 -0700

    added max file size

--- audio_gpt/settings.py
+++ audio_gpt/settings.py
@@ -27,7 +27,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
-FILE_UPLOAD_MAX_MEMORY_SIZE = 10 * 1024 * 1024
+FILE_UPLOAD_MAX_MEMORY_SIZE = 500 * 1024 * 1024
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
